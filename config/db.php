51a84e5d52ec90979b6ee04451a0559e950e68bf
Дмитрий Шиянов <dshiyanov01@gmail.com>
2022-10-03 11:45:57 +0300

    project init

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'pgsql:host=127.0.0.1;dbname=localhost;port=5432',
+    'username' => 'postgres',
+    'password' => '',
+    'charset' => 'utf8',
+];
